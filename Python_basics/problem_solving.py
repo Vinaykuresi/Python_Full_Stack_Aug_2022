c5631a8f2d7cca3089995871835bb1b75bad40fa
VinayKuresi <VinayKumar7565@gmail.com>
2022-08-30 11:25:02 +0530

    what is python, uses, interpreter, data, datatypes, Opearators, Variables(Problem Solving)

---
+++ Python_basics/problem_solving.py
@@ -0,0 +1,17 @@
+# Problem Solving :
+'''
+1. Input
+2. Process
+3. Output
+'''
+
+# input
+initial_no_of_planes = 120
+take_off_planes = 63
+landed_planes = 25
+
+# process
+current_no_of_planes = initial_no_of_planes - take_off_planes + landed_planes
+
+# output
+print("The current no of Planes on Track : ", current_no_of_planes)
---
+++ Python_basics/variables.py
@@ -0,0 +1,60 @@
+# Data types : 
+num = 10
+print(num + num)
+
+print(type(num))
+
+name = "Vinay"
+
+print(type(name))
+
+surname = " Kuresi"
+
+print(name + surname)
+print(name.upper())
+
+print(type('345'))
+
+print("ASG45HQ6")
+
+'''
+1. Numerical : 
+int -> 1234
+
+2. Numerical With Decimal Point :
+float -> 10.0, 0.345
+
+3. Alpha Numeric :
+String -> "ASG45HQ6", "Vinay", "234"
+
+4. Boolean
+Boolean -> True, False
+'''
+
+'''
+Operators : 
+
+0 -> False -> F
+1 -> True -> T
+
+And Operator : 
+A   B   A and B
+T   T   T
+T   F   F
+F   T   F
+F   F   F
+
+Or Operator : 
+A   B   A or B
+T   T   T
+T   F   T
+F   T   T
+F   F   F
+
+Not Operator : 
+A               Not(~A)
+T               F
+F               T
+not(T and F)    T
+
+'''
